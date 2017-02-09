9a3d7350265198d7b117ce15be6c3a59cdf737aa
Brad <brad@bizzartech.com>
2017-02-08 23:37:31 -0500

    lots of good updates, the options page is great!

--- rescuegroups.php
+++ rescuegroups.php
@@ -48,7 +48,7 @@ function rg_settings_init(  ) {
 
 	add_settings_section(
 		'rg_pluginPage_section', 
-		__( 'Your section description', 'wordpress' ), 
+		__( 'RescueGroups.org Portal Login Credentials', 'wordpress' ), 
 		'rg_settings_section_callback', 
 		'pluginPage'
 	);
@@ -103,24 +103,58 @@ function rg_text_field_2_render(  ) {
 
         $options = get_option( 'rg_settings' );
         ?>
-        <input type='text' name='rg_settings[rg_password]' value='<?php echo $options['rg_password']; ?>'>
+        <input type='password' name='rg_settings[rg_password]' value='<?php echo $options['rg_password']; ?>'>
         <?php
 
 }
 
 function rg_settings_section_callback(  ) { 
 
-	echo __( 'This section description', 'wordpress' );
+	echo __( '<a href="https://rescuegroups.org/manage/login">https://rescuegroups.org/manage/login</a>', 'wordpress' );
 
 }
 
 
 function rg_options_page(  ) { 
 
+	if(get_option('rg_token') == null){
+  		$options = get_option('rg_settings');
+		$rg_account = $options['rg_account'];
+		$rg_username = $options['rg_username'];
+	        $rg_password = $options['rg_password'];
+
+	        $url = "https://api.rescuegroups.org/http/";
+		$get_token_array = array('accountNumber' => $rg_account, 'username' => $rg_username, 'password' => $rg_password, 'action' => 'login');
+		$data_string = json_encode($get_token_array);                                                                                                             
+
+		$ch = curl_init($url);                                                                      
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		    'Content-Type: application/json',                                                                                
+		    'Content-Length: ' . strlen($data_string))                                                                       
+		);                                                                                                                   
+                                                                                                                     
+		$result = curl_exec($ch);
+		$result_array = json_decode($result, True);
+                if ($result_array['status'] == 'error'){
+			echo '<div class="notice notice-error"><p>'.$result_array['message'].'</p></div>';
+		} else {
+			update_option('rg_token', $result_array['data']['token']);
+		        update_option('rg_tokenhash', $result_array['data']['tokenHash']);
+		}
+	}
+
 	?>
 	<form action='options.php' method='post'>
 
 		<h2>RescueGroups.org Plugin v2</h2>
+
+        	Token: <?php echo get_option('rg_token'); ?><br/>
+	        TokenHash: <?php echo get_option('rg_tokenhash'); ?><br/><br/>
+
+		Use a non-admin, volunteer level account below to generate a secure token and tokenhash.
 
 		<?php
 		settings_fields( 'pluginPage' );
